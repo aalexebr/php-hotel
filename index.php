ecc21554bff03e5595425ec773006d88ff1c01d2
aalexebr <alex.ebr@icloud.com>
2023-08-22 15:59:19 +0200

    ADD bad table

--- index.php
+++ index.php
@@ -1,4 +1,43 @@
 <?php
+$hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
 ?>
 
 <!DOCTYPE html>
@@ -9,6 +48,88 @@
     <title>PHP Hotel</title>
 </head>
 <body>
-    
+    <header>
+        hello
+    </header>
+    <main>
+        <table>
+            <thead>
+                <tr>
+                    <?php
+                    foreach($hotels as $index => $hotel){
+                    ?>
+                        <th>
+                            <?php
+                                echo $hotel['name']; 
+                            ?>
+                        </th>
+                    <?php
+                        }
+                    ?>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach($hotels as $index => $hotel){
+                ?>
+                    <tr>
+                        <?php
+                            foreach($hotels as $index => $hotel){
+                        ?>
+                            <td>
+                                <?php
+                                    echo $hotel['description']; 
+                                ?>
+                            </td>
+                        <?php
+                            }
+                        ?>
+                    </tr>
+                    <tr>
+                        <?php
+                            foreach($hotels as $index => $hotel){
+                        ?>
+                            <td>
+                                <?php
+                                    echo $hotel['parking']; 
+                                ?>
+                            </td>
+                        <?php
+                            }
+                        ?>
+                    </tr>
+                    <tr>
+                        <?php
+                            foreach($hotels as $index => $hotel){
+                        ?>
+                            <td>
+                                <?php
+                                    echo $hotel['vote']; 
+                                ?>
+                            </td>
+                        <?php
+                            }
+                        ?>
+                    </tr>
+                    <tr>
+                        <?php
+                            foreach($hotels as $index => $hotel){
+                        ?>
+                            <td>
+                                <?php
+                                    echo $hotel['distance_to_center']; 
+                                ?>
+                            </td>
+                        <?php
+                            }
+                        ?>
+                    </tr>
+                <?php
+                    }
+                ?>
+            </tbody>
+        </table>
+        
+    </main>
 </body>
 </html>
